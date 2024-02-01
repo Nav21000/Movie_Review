ee66796d8fcc32f030409761f23cbc9d69778946
Navneet Gupta <navneetguptaend@gmail.com>
2024-02-02 02:47:11 +0530

    add dark mode button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,13 +2,16 @@ import { Navbar,TextInput,Button,Dropdown,Avatar } from 'flowbite-react'
 import {Link, useLocation} from 'react-router-dom'
 import React from 'react'
 import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
-import {useSelector} from 'react-redux'
+import {FaMoon,FaSun} from 'react-icons/fa'
+import {useSelector,useDispatch} from 'react-redux'
+import { toggleTheme } from '../redux/theme/themesSlice'
 
 
 
 export default function Header() {
     const path = useLocation().path
+    const dispatch = useDispatch();
+    const {theme} = useSelector((state)=>state.theme);
 
     const handleSignout = async () => {
         try {
@@ -56,8 +59,10 @@ export default function Header() {
        </Button>
 
        <div className="flex gap-2 md:order-2" >
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-            <FaMoon />
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())}>
+
+          {theme ==='light'? <FaMoon />:<FaSun /> }
+            
         </Button>
         {currentUser ? (
           <Dropdown
